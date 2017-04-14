29b2370cf644aa92333cd4aa23acf65692b51aa1
Alandric <acjpq4@mail.umkc.edu>
2017-04-13 22:33:13 -0500

    Update recBook.h

--- recBook.h
+++ recBook.h
@@ -20,17 +20,16 @@ class recBook
 private:
 	
 	
-	
-	template <class AJ, class JA>
+	template <typename_AJ, typname_JA>
 	void theMenu();
-	template <class A, class J>
 	void extract_keys(std::map<A, Jones> &m, std::string nameFile);
-	template <typename AJ, typename JA>
+	template<class_CJ, class_JC)
 	void read_two_column_list(std::map<A, Jones> &m, std::string nameFile);
 	int bookSearched(std::map <int, std::string> &list_book);
 	int jaccard_index_similarity(std::map <int, int> &user1, std::map <int, int> &user2);
 	bool bookUpdated(std::map <int, std::map<int, int> > ratings, int idUser, int ISBN);
-	bool itemFound(std::map<TA, KA> &m, TA itemSearched);
+	template<class_BJ, class_JB>
+	bool itemFound(std::map<BJ, JB> &m, TA itemSearched);
 	std::vector<int> recBook(std::map <int, std::map<int, int> > ratings, int idUser)
 	std::map<int, std::map<int, int> > ratings_read();
 
